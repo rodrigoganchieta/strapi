c30c33576ce773c8c13c616836f6ec550a77606a
Rodrigo Anchieta <rodrigo.anchieta@becompliance.com>
2025-03-25 10:03:44 -0300

    Fix server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,19 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
   admin: {
+    url: '/admin',
     auth: {
-        secret: env('ADMIN_JWT_SECRET')
-    }
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    allowedHosts: [
+      'localhost',
+      'strapi.bibliasagrada.digital',
+      'www.strapi.bibliasagrada.digital',
+    ],
   },
 });
