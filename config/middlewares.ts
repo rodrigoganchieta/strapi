24b47583e1f0f9a38e7ebb3be4a5f17a9c32a1ec
Rodrigo Anchieta <rodrigo.anchieta@becompliance.com>
2025-03-25 08:48:18 -0300

    first initial

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,26 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], //allow all origins
+      headers: ['*'], //allow all headers
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  admin: {
+    auth: {
+        secret: env('ADMIN_JWT_SECRET')
+    }
+  },
 });
